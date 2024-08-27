676b88ea84c5e3d61f2929f00ceb92b87dcb6685
victormeudic <victormeudic@gmail.com>
2024-08-27 12:18:26 +0200

    Fin du left panel

--- src/components/LeftPannel.tsx
+++ src/components/LeftPannel.tsx
@@ -18,6 +18,21 @@ export default function LeftPannel() {
         <Counter />
         <AddButton />
       </div>
+      <p className="text-base font-normal mt-8">
+        Free 3-5 day shipping • Tool-free assembly • 30-day trial
+      </p>
+      <div className="flex items-center mt-6 gap-36">
+        <div className="flex items-center gap-4 text-verdi-gris font-semibold">
+          <img src="/assets/heart.svg" alt="" className="color" />
+          <p>Add to Wishlist</p>
+        </div>
+        <div className="flex gap-6">
+          <img className="w-4" src="/assets/facebook.svg" alt="" />
+          <img className="w-4" src="/assets/twitter.svg" alt="" />
+          <img className="w-4" src="/assets/pinterest.svg" alt="" />
+          <img className="w-4" src="/assets/instagram.svg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
