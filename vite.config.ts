59ae1e5e47969ee9f3c4ad9e1c1e1cd936a50663
victormeudic <victormeudic@gmail.com>
2024-09-01 10:18:39 +0200

    Déploiement avec vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/spikesPageProduit/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
